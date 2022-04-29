d865323cf0955356c7665a87164dda390fccfdec
Atmoranso <atmoranso@gmail.com>
2022-04-29 12:01:22 +0300

    init: start basic-js-ds task
    - add stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+stackName = [];
+  push(el) {
+    this.stackName.push(el);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stackName.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stackName[this.stackName.length - 1];
   }
 }
 
